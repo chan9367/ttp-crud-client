93548132d7c6157837f14bfd68c6616f4477765f
Ke-vin Chan <61572013+chan9367@users.noreply.github.com>
2020-06-15 03:14:35 -0400

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import RoutesContainer from "../components/routes/RoutesContainer";
 import { NavBarContainer } from "../components/containers";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <NavBarContainer />
-        <header className="app-header">
+        <header><Link to="/campuses/new" className="add-campus">
+        Add Campus
+      </Link></header>
+        <body className="app-header">
           <RoutesContainer />
-        </header>
+        </body>
       </div>
     );
   }
